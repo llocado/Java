37e0c8440418ece42dc9468f98d07830a01f296c
Ignacio <icollado.m@gmail.com>
2021-08-17 17:51:55 -0400

    No hubo muchos cambios

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,3 +1,34 @@
 public class AddTwoNumbers {
-    
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {};
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { 
+            this.val = val; this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
 }
+
+
+    //Si p no es nulo x=p.val, si p es nulo x=0
--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
-
 public class Main{
     public static void main(String[] args){
-    
-        Triangulo triangulo = new Triangulo(3,5);
-        triangulo.area(); 
 
-        Palindrome palindrome= new Palindrome(121);
-            System.out.println(palindrome.isPalindrome);
+
 
     }
 }
