96ee842f769eeb0e4212415b5a7bd3c0c36fbcaa
Ignacio <icollado.m@gmail.com>
2021-09-07 16:53:52 -0300

    nuevo condigo de lettercombinations

--- LetterCombinationsOfPhoneNumber.java
+++ LetterCombinationsOfPhoneNumber.java
@@ -1,32 +1,39 @@
-
-import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-
-
-public class LetterCombinationsOfPhoneNumber{
+class LetterCombinationsOfPhoneNumber{
+    
     public List<String> letterCombinations(String digits) {
-        if (digits.equals("")) return new ArrayList<String>();
-        List<String> res=new ArrayList<>();
-        String[] map={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        StringBuilder sb= new StringBuilder();
-        recursion(digits,res,sb,map,0);
+        if(digits == null || digits.length() == 0) return new ArrayList<>();
+        
+        Map<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+        
+        List<String> res = new ArrayList<>();
+        dfs(0, digits, new StringBuilder(), res, map);
         return res;
     }
-    void recursion(String digits,List<String> res,StringBuilder sb,String[] map,int idx){
-        if(idx>=digits.length()){
+    
+    private void dfs(int st, String digits, StringBuilder sb, List<String> res, Map<Character, String> map) {
+        if(st == digits.length()) {
             res.add(sb.toString());
             return;
         }
-        else{
-            int digit = digits.charAt(idx) - '0';
-            String str=map[digit-2];
-            for(int i=0;i<str.length();i++){
-                char c=str.charAt(i);
-                sb.append(c);
-                recursion(digits,res,sb,map,idx+1);
-                sb.setLength(sb.length()-1);
-            }
+        
+        String str = map.get(digits.charAt(st));
+        for(int i = 0; i < str.length(); i++) {
+            sb.append(str.charAt(i));
+            dfs(st + 1, digits, sb, res, map);
+            sb.setLength(sb.length() - 1);
         }
     }
 }
