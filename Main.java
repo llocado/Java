09e2cbbc9dc8e5f86c69c837a79095033b2e0b48
Ignacio <icollado.m@gmail.com>
2021-09-07 16:54:09 -0300

    nuevo cambio de main

--- Main.java
+++ Main.java
@@ -1,28 +1,11 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main{
     public static void main(String[] args){
 
-        ArrayList<ArrayList<String>> lista2=new ArrayList<ArrayList<String>>();
+        LetterCombinationsOfPhoneNumber x = new LetterCombinationsOfPhoneNumber();
 
-        ArrayList<String> subLista =new ArrayList<String>();
-        System.out.println(subLista.isEmpty());
-        subLista.add("HOLA");
-        subLista.add("COMO");
-        subLista.add("ESTAS");
-
-        lista2.add(subLista);
-
-        System.out.println(lista2);
-
-        lista2.add(subLista);
-
-        System.out.println(lista2);
-
-        System.out.println(lista2.get(0).add("de nuevo"));
-
-        System.out.println(lista2);
+        x.letterCombinations("123");
 
     }
 }    
